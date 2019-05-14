af0ba99ad5bf9b8034b2e662a69f1396f0ee23c9
Joseph Trubenstein <josephtrubenstein@gmail.com>
2019-05-14 12:36:52 -0400

    about page

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var vocabPusher =  require('../routes/users/utils/vocabPusher')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sushi Script', subtitle: 'Learn Sushi Language with Javascript' });
+  res.render('index', { title: 'Sushi Script', subtitle: 'Learning Japanese with Javascript' });
 });
 
 /* GET api page. */
-router.get('/api', function(req, res, next) {
-  res.render('API', { title: 'Express' });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
 });
 router.get('/flash-card', function(req, res, next) {
   res.render('flash-card', { title: 'Sushi Script'});
